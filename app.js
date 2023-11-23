28294d7651f4c07b92bf0e2404cd9bbbdd812544
Ryan Clarian <rclarian@gmail.com>
2023-11-23 13:40:43 +0800

    Added the isfav class
    Added the isfav class

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
-                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
-                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg' },
+                { title: 'Name of the Wind', author: 'Patrick Rothfuss', img: 'assets/1.jpg', isFav: true },
+                { title: 'The Way of Kings', author: 'Brandon Sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'The Final Empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isFav: true },
             ]
         }
     },
